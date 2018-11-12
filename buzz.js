956ed566253fb871aa337ec4f381acdda0296714
Andres.C <andrescabrera@pursuit.org>
2018-11-12 16:25:31 -0500

    submit button ok

--- buzz.js
+++ buzz.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector(".form");
+  let submit = document.querySelector(".submit");
+  let pickAgain = document.querySelector(".pickAgain");
   let answer = 0;
-  form.addEventListener("click", event => {
-    answer += +event.target.value;
-    console.log(answer);
-  });
 
+  form.addEventListener("change", event => {
+
+    answer += +event.target.value;
+
+    // console.log(answer)
+    // console.log("It keeps adding the value without stopping if you click back and forth too much");;
+  });
+  submit.addEventListener('click', (event) => {
+      answer += +event.target.value;
+      console.log(answer);
+ });
 });
